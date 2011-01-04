d655b9dc5ad660e724a0a9c576f1043e64f79f21
Ralph Loader <suckfish@ihug.co.nz>
2011-01-05 10:22:14 +1300

    Infer the dsp.  Use signed/unsigned not std_logic_vector

--- downconvert.vhd
+++ downconvert.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+--use IEEE.STD_LOGIC_ARITH.ALL;
+--use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 library work;
 use work.sincos.all;
@@ -16,11 +16,11 @@ library UNISIM;
 use UNISIM.VComponents.all;
 
 entity downconvert is
-    Port (data : in  STD_LOGIC_VECTOR (13 downto 0);
-          qq   : out STD_LOGIC_VECTOR (17 downto 0);
-          ii   : out STD_LOGIC_VECTOR (17 downto 0);
+    Port (data : in  signed (13 downto 0);
+          qq   : out unsigned18;
+          ii   : out unsigned18;
           clk  : in  STD_LOGIC;
-          freq : in  STD_LOGIC_VECTOR (23 downto 0));
+          freq : in  unsigned (23 downto 0));
 end downconvert;
 
 architecture Behavioral of downconvert is
@@ -46,12 +46,12 @@ architecture Behavioral of downconvert is
   -- output is signed 2^28/pi^2, [just under] 25 bits plus sign.
   -- e.g., accumulating over 1024 samples 35 bits plus sign.
 
-  signal index_acc : std_logic_vector(23 downto 0);
+  signal index_acc : unsigned(23 downto 0);
 
-  signal cos_index : std_logic_vector(9 downto 0);
-  signal sin_index : std_logic_vector(9 downto 0);
-  signal sin_low : std_logic_vector(1 downto 0);
-  signal sin_low_2 : std_logic_vector(1 downto 0);
+  signal cos_index : unsigned(9 downto 0);
+  signal sin_index : unsigned(9 downto 0);
+  signal sin_low : unsigned(1 downto 0);
+  signal sin_low_2 : unsigned(1 downto 0);
 
   signal cos_minus : std_logic;
   signal sin_minus : std_logic;
@@ -61,20 +61,34 @@ architecture Behavioral of downconvert is
   signal sin_minus_3 : std_logic;
   signal cos_minus_4 : std_logic;
   signal sin_minus_4 : std_logic;
-  signal cos_opmode : std_logic_vector(7 downto 0) := "00011001";
-  signal sin_opmode : std_logic_vector(7 downto 0) := "00011001";
+  signal cos_minus_5 : std_logic;
+  signal sin_minus_5 : std_logic;
+  signal cos_minus_6 : std_logic;
+  signal sin_minus_6 : std_logic;
 
-  signal packed_cos : word18;
-  signal packed_sin : word18;
+  signal packed_cos : unsigned18;
+  signal packed_sin : unsigned18;
 
-  signal cos_main : word18 := "00" & x"0000";
-  signal sin_main : word18 := "00" & x"0000";
-  signal cos_offset : word18 := "00" & x"0000";
-  signal sin_offset : word18 := "00" & x"0000";
+  subtype signed18 is signed(17 downto 0);
 
-  signal data18 : word18;
-  signal qq_buf : std_logic_vector(47 downto 0);
-  signal ii_buf : std_logic_vector(47 downto 0);
+  signal cos_main : unsigned18;
+  signal sin_main : unsigned18;
+  signal cos_offset : unsigned18;
+  signal sin_offset : unsigned18;
+  signal cos_main_1 : unsigned18;
+  signal sin_main_1 : unsigned18;
+  signal cos_offset_1 : unsigned18;
+  signal sin_offset_1 : unsigned18;
+  signal sin : signed18;
+  signal cos : signed18;
+
+  signal data_1 : signed(13 downto 0);
+  signal data_2 : signed(13 downto 0);
+  signal qq_prod : signed(31 downto 0);
+  signal ii_prod : signed(31 downto 0);
+
+  signal qq_buf : signed(47 downto 0);
+  signal ii_buf : signed(47 downto 0);
 
   signal sintable : sinrom_t := sinrom;
 
@@ -100,134 +114,58 @@ begin
       sin_minus <= index_acc(23);
 
       -- Lookup the sin and cos tables.
-      packed_cos <= sintable(conv_integer(cos_index));
-      packed_sin <= sintable(conv_integer(sin_index));
+      packed_cos <= sintable(to_integer(cos_index));
+      packed_sin <= sintable(to_integer(sin_index));
       cos_minus_2 <= cos_minus;
       sin_minus_2 <= sin_minus;
       sin_low_2 <= sin_low;
 
       -- Prepare the sin and cos.
-      cos_main(13 downto 0) <= packed_cos(13 downto 0);
-      sin_main(13 downto 0) <= packed_sin(13 downto 0);
-      cos_offset(2 downto 0) <= sinoffset(packed_cos, not sin_low_2);
-      sin_offset(2 downto 0) <= sinoffset(packed_sin, sin_low_2);
+      cos_main <= packed_cos and "00" & x"3fff";
+      sin_main <= packed_sin and "00" & x"3fff";
+      cos_offset <= resize(sinoffset(packed_cos, not sin_low_2), 18);
+      sin_offset <= resize(sinoffset(packed_sin, sin_low_2), 18);
       cos_minus_3 <= cos_minus_2;
       sin_minus_3 <= sin_minus_2;
+      data_1 <= data;
 
-      -- The opmode bit to control the final plus/minus only has 1 cycle of
-      -- buffering in the DSP block, while through the adder & multiplier has
-      -- 3 cycles.  So 2 more cycles here...
+      -- Buffer.
+      cos_main_1 <= cos_main;
+      sin_main_1 <= sin_main;
+      cos_offset_1 <= cos_offset;
+      sin_offset_1 <= sin_offset;
       cos_minus_4 <= cos_minus_3;
       sin_minus_4 <= sin_minus_3;
-      cos_opmode(7) <= cos_minus_4;
-      sin_opmode(7) <= sin_minus_4;
+
+      -- Pre-add.
+      cos <= signed(cos_main_1 + cos_offset_1);
+      sin <= signed(sin_main_1 + sin_offset_1);
+      data_2 <= data_1;
+      cos_minus_5 <= cos_minus_4;
+      sin_minus_5 <= sin_minus_4;
+
+      -- Multiply
+      qq_prod <= data_2 * cos;
+      ii_prod <= data_2 * sin;
+      cos_minus_6 <= cos_minus_5;
+      sin_minus_6 <= sin_minus_5;
+
+      -- Post add.
+      if cos_minus_6 = '1' then
+        qq_buf <= qq_buf - qq_prod;
+      else
+        qq_buf <= qq_buf + qq_prod;
+      end if;
+      if sin_minus_6 = '1' then
+        ii_buf <= ii_buf - ii_prod;
+      else
+        ii_buf <= ii_buf + ii_prod;
+      end if;
 
       -- Output
-      qq <= qq_buf(35 downto 18);
-      ii <= ii_buf(35 downto 18);
+      qq <= unsigned(qq_buf(35 downto 18));
+      ii <= unsigned(ii_buf(35 downto 18));
 
     end if;
   end process;
-
-  data18(17) <= data(13);
-  data18(16) <= data(13);
-  data18(15) <= data(13);
-  data18(14) <= data(13);
-  data18(13 downto 0) <= data;
-
-  qq_dsp : DSP48A1
-    generic map (
-      A0REG => 1,
-      A1REG => 1,
-      B0REG => 1,
-      B1REG => 1,
-      CARRYINREG => 1,
-      CARRYINSEL => "OPMODE5",
-      CREG => 1,
-      DREG => 1,
-      MREG => 1,
-      OPMODEREG => 1,
-      PREG => 1, -- Register output.
-      RSTTYPE => "SYNC") -- Don't use it anyway.
-    port map (
-      BCOUT=> open,
-      M=> open,
-      PCOUT=> open,
-      P=> qq_buf,
-      CARRYOUT=> open,
-      CARRYOUTF=> open,
-
-      A=> data18,
-      B=> cos_offset,
-      C=> x"000000000000",
-      D=> cos_main,
-      OPMODE=> cos_opmode, -- ?/add / carry0 / pre-add / P / mult.
-      PCIN=> x"000000000000",
-      CARRYIN=> '0',
-      CEA=> '1',
-      CEB=> '1',
-      CEC=> '1',
-      CECARRYIN=>'0', -- Carry in reg not used.
-      CED=> '1',
-      CEM=> '1',
-      CEOPMODE=> '1',
-      CEP=> '1',
-      CLK=> Clk,
-      RSTA=> '0',
-      RSTB=> '0',
-      RSTC=> '0',
-      RSTCARRYIN=> '0',
-      RSTD=> '0',
-      RSTM=> '0',
-      RSTOPMODE=> '0',
-      RSTP=> '0');
-
-  ii_dsp : DSP48A1
-    generic map (
-      A0REG => 1,
-      A1REG => 1,
-      B0REG => 1,
-      B1REG => 1,
-      CARRYINREG => 1,
-      CARRYINSEL => "OPMODE5",
-      CREG => 1,
-      DREG => 1,
-      MREG => 1,
-      OPMODEREG => 1,
-      PREG => 1, -- Register output.
-      RSTTYPE => "SYNC") -- Don't use it anyway.
-    port map (
-      BCOUT=> open,
-      M=>open,
-      PCOUT=> open,
-      P=> ii_buf,
-      CARRYOUT=> open,
-      CARRYOUTF=> open,
-
-      A=> data18,
-      B=> sin_offset,
-      C=> x"000000000000",
-      D=> sin_main,
-      OPMODE=> sin_opmode, -- ?/add / carry0 / pre-add / P / mult.
-      PCIN=> x"000000000000",
-
-      CARRYIN=> '0',
-      CEA=> '1',
-      CEB=> '1',
-      CEC=> '1',
-      CECARRYIN=>'0', -- Carry in reg not used.
-      CED=> '1',
-      CEM=> '1',
-      CEOPMODE=> '1',
-      CEP=> '1',
-      CLK=> Clk,
-      RSTA=> '0',
-      RSTB=> '0',
-      RSTC=> '0',
-      RSTCARRYIN=> '0',
-      RSTD=> '0',
-      RSTM=> '0',
-      RSTOPMODE=> '0',
-      RSTP=> '0');
-
 end Behavioral;
--- sinrom.c
+++ sinrom.c
@@ -1,10 +1,8 @@
-// The sinrom has sines and derivatives from one quadrant on 0..1023.
-// We store the sine and an approximation to the derivative; this enables
-// us to get 2048 samples per quadrant, i.e., 0..2047.
-// The index is the middle of the angle range.  So 0 is pi/8192 radians.
-// 1 is 3pi/8192 radians, 2047 is 4095pi/8192 radians.
-// We store the event numbered samples, and the differences to give the even
-// numbered values.
+// The sinrom has sines and derivatives from one quadrant stored in a 1024
+// entry table.  Each entry stores 4 consecutive entires:
+// The base entry (14 bits precision).
+// The incremental deltas to each of the next 3 entries, this can be packed
+// into 4 bits.
 
 // We don't want too much precision.  We choose the scale so that the first few
 // items go 1,3,5,7 etc.
@@ -12,9 +10,56 @@
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    double mult = 8192 / M_PI;
+static int dddeltas[16] = {
+    0000, // 0 0 0
+    0001, // 1 1 1
+    0010, // 1 1 0
+    0011, // 2 2 1
+    0012, // 3 3 2, doesn't actually happen.
+    0100, // 1 0 0
+    0101, // 2 0 1
+    0110, // 2 1 0
+    0111, // 3 2 1
+    0112, // 4 3 2
+    0121, // 4 3 1
+    0122, // 5 4 2
+    //0210, // 3 1 0 Not possible...
+    0211, // 4 2 1
+    0212, // 5 3 2
+    0221, // 5 3 1
+    0222  // 6 4 2
+};
+
+
+static int ddd(int deltas)
+{
+    for (int i = 0; i != 16; ++i)
+        if (deltas == dddeltas[i])
+            return i;
+    abort();
+}
+
+
+int main(void)
+{
+    double scale = 16384 / M_PI;
+    double omega = M_PI / 16384;
+
+    int used = 0;
+
+    printf ("library IEEE;\n"
+            "use IEEE.NUMERIC_STD.ALL;\n"
+            "\n"
+            "package sincos is\n"
+            "subtype unsigned18 is unsigned(17 downto 0);\n"
+            "subtype unsigned2 is unsigned(1 downto 0);\n"
+            "subtype unsigned3 is unsigned(2 downto 0);\n"
+            "function sinoffset(sinent : unsigned18; lowbits : unsigned2) return unsigned3;\n"
+            "type sinrom_t is array (0 to 1023) of unsigned18;\n"
+            "constant sinrom : sinrom_t := (\n");
+
     for (int i = 0; i != 1024; ++i) {
         if (i & 3)
             printf (", ");
@@ -23,20 +68,69 @@ int main() {
         else
             printf ("    ");
 
-        double c = sin ((i * 4 + 1) * (M_PI / 8192)) * mult;
-        double d = sin ((i * 4 + 3) * (M_PI / 8192)) * mult;
-        int u = c + 0.5;
-        int v = d + 0.5;
-        assert (c - u < 0.5 && u - c < 0.5);
-        assert (d - v < 0.5 && v - d < 0.5);
-        assert (u <= v);
-        assert (v <= u + 2);
-        assert (0 <= u);
-        assert (v < 4096);
-        printf ("\"%i%i\"&x\"%x%03x\"",
-                0, 0,
-                v - u,
-                u & 65535);
+        double c0 = sin ((i * 8 + 1) * omega) * scale;
+        double c1 = sin ((i * 8 + 3) * omega) * scale;
+        double c2 = sin ((i * 8 + 5) * omega) * scale;
+        double c3 = sin ((i * 8 + 7) * omega) * scale;
+        int v0 = c0 + 0.5;
+        int v1 = c1 + 0.5;
+        int v2 = c2 + 0.5;
+        int v3 = c3 + 0.5;
+
+        assert (c0 - v0 <= 0.5);
+        assert (c1 - v1 <= 0.5);
+        assert (c2 - v2 <= 0.5);
+        assert (c3 - v3 <= 0.5);
+        assert (v0 - c0 <= 0.5);
+        assert (v1 - c1 <= 0.5);
+        assert (v2 - c2 <= 0.5);
+        assert (v3 - c3 <= 0.5);
+        assert (v0 >= 0);
+        assert (v0 < 16384);
+
+        int delta1 = v1 - v0;
+        int delta2 = v2 - v1;
+        int delta3 = v3 - v2;
+
+        assert (delta1 >= 0);
+        assert (delta2 >= 0);
+        assert (delta3 >= 0);
+        assert (delta1 <= 2);
+        assert (delta2 <= 2);
+        assert (delta3 <= 2);
+
+        int index = ddd(delta3 * 0100 + delta2 * 0010 + delta1);
+        used |= 1 << index;
+        printf ("\"%i%i\"&x\"%04x\"",
+                (index & 8) != 0, (index & 4) != 0, (index & 3) * 16384 + v0);
     }
-    printf ("\n");
+    printf (");\n\n");
+    printf ("-- Used bitmask: %0x\n", used);
+    printf ("end sincos;\n"
+            "\n"
+            "package body sincos is\n"
+            "function sinoffset(sinent : unsigned18; lowbits : unsigned2) return unsigned3 is\n"
+           "begin\n"
+           "    case lowbits & sinent(17 downto 14) is\n");
+
+    for (int lowbits = 0; lowbits < 4; ++lowbits)
+        for (int index = 0; index != 16; ++index) {
+            int ddd = dddeltas[index];
+            if (lowbits == 0 && ddd == 0)
+                continue;
+            int ccc = (ddd << 9) + (ddd << 6) + (ddd << 3);
+            ccc &= 07770;
+            int c = ccc >> (3 * lowbits);
+            printf (
+                "    when \"%i%i\" & x\"%x\" => return \"%i%i%i\"; -- %03o %04o\n",
+                lowbits >> 1, lowbits & 1, index,
+                (c & 4) != 0, (c & 2) != 0, c & 1, ddd, ccc);
+        }
+
+    printf ("    when others => return \"000\";\n"
+            "    end case;\n"
+            "end sinoffset;\n"
+            "end sincos;\n");
+
+    return 0;
 }
