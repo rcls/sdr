4bcf6fde8341bb40493ec1bd8b722b6b5c43ae00
Ralph Loader <suckfish@ihug.co.nz>
2011-01-05 16:29:33 +1300

    Merge the two multifilter rams into one. Fixes the ram usage.

--- multifilter.vhd
+++ multifilter.vhd
@@ -18,34 +18,25 @@ entity multifilter is
 end;
 
 architecture Behavioral of multifilter is
-  type ram_t is array(0 to 255) of signed36;
-  signal ram_new : ram_t;
-  signal ram_old : ram_t;
-  signal buf_new : signed36;
-  signal buf_new2 : signed36;           -- FIXME, use multicycle constraint.
-  signal buf_old : signed36;
-  signal buf_old2 : signed36;           -- FIXME, use multicycle constraint.
-  signal index : unsigned(7 downto 0);
+  type ram_t is array(0 to 512) of signed36;
+  signal ram : ram_t;
+  signal buf : signed36;
+  signal ramout : signed36;
+  signal count : unsigned(9 downto 0);
+  signal index : unsigned(8 downto 0);
   alias switch : std_logic is index(0);
-  signal phase : unsigned(3 downto 0);
+  alias phase : unsigned(1 downto 0) is count(1 downto 0);
 
-  signal diff_new : signed36;
+  signal diff : signed36;
 
 begin
+  index <= count(9 downto 2) & count(0);
+
   process (Clk)
     variable dd : signed36;
   begin
     if Clk'event and Clk = '1' then
-      phase(3 downto 1) <= phase(2 downto 0);
-      if phase(2 downto 0) = x"000" then
-        phase(0) <= '1';
-      else
-        phase(0) <= '0';
-      end if;
-
-      if phase(3) = '1' then
-        index <= index + 1;
-      end if;
+      count <= count + 1;
 
       if switch = '0' then
         dd := in0;
@@ -53,24 +44,24 @@ begin
         dd := in1;
       end if;
 
-      -- Only wanted on phase 0.
-      buf_new <= ram_new(to_integer(index));
+      -- Only wanted on phase 0 and 1.
+      buf <= ram(to_integer(index));
 
-      -- Only wanted on phase 1.
-      buf_new2 <= buf_new;
-      buf_old <= ram_old(to_integer(index));
+      -- Only wanted on phase 1 and 2.
+      ramout <= buf;
 
-      -- Only wanted on phase 2.
-      buf_old2 <= buf_old;
-      diff_new <= dd - buf_new2;
-      if phase(2) = '1' then
-        ram_new(to_integer(index)) <= dd;
+      -- Only need to do in one of phase 0&1 but doing it on both is harmless.
+      if phase(1) = '0' then
+        diff <= dd;
       end if;
 
-      -- Only wanted on phase 3.
-      if phase(3) = '1' then
-        qq <= diff_new - buf_old2;
-        ram_old(to_integer(index)) <= diff_new;
+      if phase(1) = '1' then
+        diff <= diff - ramout;
+        ram(to_integer(index)) <= diff;
+      end if;
+
+      if phase = "00" then
+        qq <= diff;
       end if;
 
     end if;
