0caf78de9b6de7756b19b11e98df9caa132ae450
Ralph Loader <suckfish@ihug.co.nz>
2013-03-15 10:24:30 +1300

    Remove obsolete registers.

--- lib/registers.h
+++ lib/registers.h
@@ -27,7 +27,6 @@
 #define XMIT_BANDPASS 16
 #define XMIT_BURST 20
 #define XMIT_CPU_SSI 24
-#define XMIT_IDLE 28
 
 #define XMIT_TURBO 64
 #define XMIT_LOW_LATENCY 128
@@ -36,14 +35,5 @@
 #define FLASH_DATA 1
 #define FLASH_CS 2
 #define FLASH_CLK 4
-#define FLASH_RECV 1
-#define FLASH_XMIT 8
-#define FLASH_OVERRUN 128
-
-#define CPU_SSI_DATA 1
-#define CPU_SSI_FSS 2
-#define CPU_SSI_CLK 4
-#define CPU_SSI_FSS_STRETCH 64
-#define CPU_SSI_CPU_IS_MASTER 128
 
 #endif
