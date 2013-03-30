9d8201fee3c30fb285d43f8ab7f9b80913d08032
Ralph Loader <suckfish@ihug.co.nz>
2013-03-31 11:44:23 +1300

    Smarter command parsing.

--- util/command.c
+++ util/command.c
@@ -12,6 +12,7 @@ unsigned char raw[4096];
 static void basic(const char * comm)
 {
     usb_printf("%s\n", comm);
+
     unsigned got;
     unsigned lastnz;
     do {
@@ -27,14 +28,45 @@ static void basic(const char * comm)
 }
 
 
+static bool havechar(int argc, char * argv[], char c)
+{
+    for (int i = 1; i != argc; ++i)
+        if (strchr(argv[i], c) != NULL)
+            return true;
+    return false;
+}
+
+
+static void slashed(const char * p)
+{
+    for (; *p; p++)
+        if (*p == '/')
+            basic("\n");
+        else
+            usb_printf("%c", *p);
+}
+
+
 int main(int argc, char * argv[])
 {
     usb_open();
     usb_flush();
 
     if (argc > 1) {
-        for (int i = 1; i < argc; ++i)
-            basic(argv[i]);
+        if (havechar(argc, argv, '/') || !havechar(argc, argv, ' ')) {
+            for (int i = 1; i < argc; ++i) {
+                if (i != 1)
+                    usb_printf(" ");
+                slashed(argv[i]);
+            }
+            const char * last = argv[argc - 1];
+            if (*last && last[strlen(last) - 1] != '/')
+                basic("\n");
+        }
+        else {
+            for (int i = 1; i < argc; ++i)
+                basic(argv[i]);
+        }
         return 0;
     }
 
